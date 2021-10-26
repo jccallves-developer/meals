f8c39218edeadcbc9e7d722b1d4cbed146713279
jccallves <jccallves@macae.ufrj.br>
2021-10-26 12:37:39 -0300

    #Aula184 - Tela de Categorias

---
+++ lib/components/category_item.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'package:meals/models/category.dart';
+
+class CategoryItem extends StatelessWidget {
+  final Category category;
+
+  const CategoryItem({
+    Key? key,
+    required this.category,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(category.title);
+  }
+}
